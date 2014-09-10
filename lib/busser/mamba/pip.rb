cbe03659c735c3cdc5b7918abb82dc701e35eb48
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2014-09-10 21:48:34 +0200

    Ensure pip is installed

---
+++ lib/busser/mamba/pip.rb
@@ -0,0 +1,27 @@
+# -*- encoding: utf-8 -*-
+
+module Busser
+  module Mamba
+    # Common methods for pip
+    #
+    # @author Jaime Gil de Sagredo <jaimegildesagredo@gmail.com>
+    #
+    module Pip
+      module_function
+
+      def ensure_pip
+        run!(ensure_pip_cmd)
+      end
+
+      def ensure_pip_cmd
+        <<-CMD
+        pip || curl https://bootstrap.pypa.io/get-pip.py|python
+        CMD
+      end
+
+      def pip_install(requirements)
+        run!("pip install #{requirements}")
+      end
+    end
+  end
+end
--- lib/busser/runner_plugin/mamba.rb
+++ lib/busser/runner_plugin/mamba.rb
@@ -1,25 +1,37 @@
 # -*- encoding: utf-8 -*-
 
 require 'busser/runner_plugin'
+require 'busser/mamba/pip'
 
-# A Busser runner plugin for Mamba.
-#
-# @author Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
-#
-class Busser::RunnerPlugin::Mamba < Busser::RunnerPlugin::Base
-  postinstall do
-    run!(
-      "pip install -e git+git://github.com/nestorsalceda/mamba.git#egg=mamba")
+module Busser
+  module Thor
+    class BaseGroup
+      include Busser::Mamba::Pip
+    end
   end
 
-  def test
-    suite_path = suite_path('mamba').to_s
-    requirements_file = File.join(suite_path, 'requirements.txt')
+  module RunnerPlugin
+    # A Busser runner plugin for Mamba.
+    #
+    # @author Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
+    #
+    class Mamba < Base
+      postinstall do
+        ensure_pip
+        pip_install(
+          '-e git+git://github.com/nestorsalceda/mamba.git#egg=mamba')
+      end
 
-    if File.exists?(requirements_file)
-      run!("pip install -r #{requirements_file}")
+      def test
+        suite_path = suite_path('mamba').to_s
+        requirements_file = File.join(suite_path, 'requirements.txt')
+
+        if File.exists?(requirements_file)
+          run!("pip install -r #{requirements_file}")
+        end
+
+        run!("mamba #{suite_path}")
+      end
     end
-
-    run!("mamba #{suite_path}")
   end
 end
