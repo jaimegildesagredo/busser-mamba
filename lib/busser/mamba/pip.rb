e80eb454b81ceb4cdace71602736e1a43f56796d
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2014-09-11 23:31:15 +0200

    Does not depend on curl to install pip

--- lib/busser/mamba/pip.rb
+++ lib/busser/mamba/pip.rb
@@ -10,13 +10,18 @@ module Busser
       module_function
 
       def ensure_pip
-        run!(ensure_pip_cmd)
+        if !pip_installed?
+          f = Tempfile.new('busser-mamba', Dir.tmpdir, 'wb+')
+          f.write(Net::HTTP.get(URI('https://bootstrap.pypa.io/get-pip.py')))
+          f.flush
+          run!("python #{f.path}")
+          f.close!
+        end
       end
 
-      def ensure_pip_cmd
-        <<-CMD
-        pip --version || curl https://bootstrap.pypa.io/get-pip.py|python
-        CMD
+      def pip_installed?
+        system('pip --version')
+        $?.exitstatus == 0
       end
 
       def pip_install(requirements)
